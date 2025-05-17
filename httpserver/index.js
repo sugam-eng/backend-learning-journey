ee30e97bd4d0512b786cb7a1ac011064bb0f8479
sugam-eng <sugamkolalenagaraj@gmail.com>
2025-05-17 14:19:36 +0530

    building server logic for different path

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,7 +1,17 @@
 const http=require("http") //It is a core module (no need to install it).Lets you build web servers and handle HTTP requests/responses.
 const myserver=http.createServer((req,res)=>{
     console.log("new req")
-    res.end("hello from sugam")
+    console.log("Requested Path:", req.url);
+    switch (req.url){
+        case "/":res.end("hello from sugam")
+        break
+        case "/about":res.end("you are in about section")
+        break
+        case "/home":res.end("you are in homepage")
+        default:
+            res.end("404 not found")
+    }
+    
 })//Creates a server that handles incoming requests
 
 myserver.listen(8000)
