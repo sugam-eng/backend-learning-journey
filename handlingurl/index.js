c91526e182e4577d0eb3f96e37f44d8725d2a425
sugam-eng <sugamkolalenagaraj@gmail.com>
2025-05-18 23:00:50 +0530

    succesfully understood basic server working in accordance with the url

---
+++ handlingurl/index.js
@@ -0,0 +1,25 @@
+const http=require("http")
+const url=require("url") //here url is the external library installed using npm i url
+
+const myserver=http.createServer((req,res)=>{
+    const myurl=url.parse(req.url,true);
+    console.log(myurl) //here myurl is the object which contains all the information present in url, basically it breaks the whole url
+    switch(myurl.pathname){
+        case "/":
+            res.end("homepage")
+            break
+        case "/about":
+            const qp=myurl.query.myname // just pass true in line 5, this creates query object with keys from url keys and values from url value
+            res.end(`hi ${qp}`) // assume my url is localhost:8000/about?myname=sugam&userid=1
+            break
+
+        //for example in youtube
+        case "/search":
+            const search=myurl.query.search_query
+            res.end("here are your results for"+search) //assuming yt url as ..../search?search_query=trending+news
+        default:
+            res.end("404 end")
+    }
+
+})
+myserver.listen(8000);
