fbc6e6d03156adf61abfc49e7255167b03ee7f52
sugam-eng <sugamkolalenagaraj@gmail.com>
2025-05-23 12:54:56 +0530

    successfully built hybrid server

--- 06-rest-api/index.js
+++ 06-rest-api/index.js
@@ -7,9 +7,18 @@ const app=express()
 const port=8000;
 
 //routes
-app.get("/users", (req,res)=>{
+app.get("/api/users", (req,res)=>{
     return res.json(users)
 })
 //but we should build server that should be hybrid in nature. i.e our server should send json data for mobile applications and html data for browser.
+
+app.get("/users", (req,res)=>{
+    const html=`
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html)
+})//hybrid server sends html data if path is /users, sends json data if path is /api/users
 
 app.listen(port,()=>console.log(`server started at port: ${port}`))
