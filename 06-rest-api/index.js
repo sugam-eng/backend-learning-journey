d2682c3cd6884a4bcf6e19ab88b1370c835de644
sugam-eng <sugamkolalenagaraj@gmail.com>
2025-05-23 12:32:30 +0530

    created basic server which returns json data

--- 06-rest-api/index.js
+++ 06-rest-api/index.js
@@ -2,8 +2,13 @@
 //install express
 
 const express=require('express')
+const users=require('./MOCK_DATA.json')
 const app=express()
-
 const port=8000;
+
+//routes
+app.get("/users", (req,res)=>{
+    return res.json(users)
+})
 
 app.listen(port,()=>console.log(`server started at port: ${port}`))
