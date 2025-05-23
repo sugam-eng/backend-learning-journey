5b9496c1253726b6119c11e9f5c24fd1b06586cb
sugam-eng <sugamkolalenagaraj@gmail.com>
2025-05-23 12:41:32 +0530

    trying to build hybrid server

--- 06-rest-api/index.js
+++ 06-rest-api/index.js
@@ -10,5 +10,6 @@ const port=8000;
 app.get("/users", (req,res)=>{
     return res.json(users)
 })
+//but we should build server that should be hybrid in nature. i.e our server should send json data for mobile applications and html data for browser.
 
 app.listen(port,()=>console.log(`server started at port: ${port}`))
